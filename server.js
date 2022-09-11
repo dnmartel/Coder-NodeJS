b26bf3b7c96fdde3dd954fc57517f77191daeeac
dnmartel <marteld1992@gmail.com>
2022-09-11 14:56:11 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+
+const fs = require('fs');
+
+class Contenedor {
+
+    constructor(nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    // save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+    async save(obj) {
+        try {
+            //Traigo el contenido del archivo y lo parseo
+            let contenido = await this.getAll();
+            //Asigno ID 1 si no tiene contenido, sino, 1 más que el último
+            if (contenido.length === 0) {
+                obj.id = 1;
+            } else {
+                obj.id = (contenido[contenido.length - 1].id) + 1;
+            };
+            //Pusheo el objeto al array
+            contenido.push(obj);
+            //Escribo el objeto
+            await fs.promises.writeFile(`./${this.nombreArchivo}`, JSON.stringify(contenido, null, 2))
+
+            return (' Objeto guardado correctamente 😁', `\n Ruta del archivo: ./${this.nombreArchivo}\n`, obj);
+        } catch (error) {
+            console.log("😢 No se pudo guardar el objeto: " + error)
+        }
+    }
+
+    // getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
+    async getById(id) {
+        if (id) {
+            try {
+                //Traigo el contenido del archivo y lo parseo
+                let contenido = await this.getAll()
+                //Retorno el elementro filtrado por ID
+                return contenido.filter(el => el.id === id)
+            } catch (error) {
+                console.log('😢 No se pudo leer el id: ' + error)
+            }
+        } else {
+            return null
+        }
+
+    }
+
+    // getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
+    async getAll() {
+        try {
+            //Leo el archivo y lo almaceno en una variable
+            const contenidoArchivo = await fs.promises.readFile(`./${this.nombreArchivo}`, 'utf-8')
+            //Retorno el contenido del archivo
+            return (JSON.parse(contenidoArchivo));
+        } catch (error) {
+            return []
+        }
+    }
+
+    // deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
+    async deleteById(id) {
+
+        if (id) {
+            try {
+                let contenido = await this.getAll()
+
+                //Busco el indice del elemento por ID
+                let indexID = contenido.findIndex(item => item.id === id);
+                //Lo quito del Array
+                contenido.splice(indexID, 1);
+                //Reescribo el archivo
+                await fs.promises.writeFile(`./${this.nombreArchivo}`, JSON.stringify(contenido, null, 2))
+                return ('Se elimino el elemento con el id ' + id)
+            } catch (error) {
+                console.log('😢 No se pudo leer el id: ' + error)
+            }
+        } else {
+            return null
+        }
+
+    }
+
+    // deleteAll(): void - Elimina todos los objetos presentes en el archivo.
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(`./${this.nombreArchivo}`, "[]")
+            return ('Todos los elementos borrados');
+        } catch (error) {
+            console.log(`\n😢 No se pudo borrar el contenido de: "./${this.nombreArchivo}":\n ${error} \n\n`)
+        }
+    }
+
+}
+
+let datosContenedor = new Contenedor("productos.txt");
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+})
+server.on('error', error => console.log(error))
+
+app.get('/productos', async (req, res) => {
+
+    res.json(await datosContenedor.getAll())
+})
+
+app.get('/productoRandom', async (req, res) => {
+
+    const arrProductos = await datosContenedor.getAll();
+    let randBetween = Math.floor(Math.random() * arrProductos.length) + 1;
+
+    res.json(await datosContenedor.getById(randBetween))
+})
