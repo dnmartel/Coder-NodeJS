2ab9bebbf68db1584f674d51ef32dc12b7c8e9f0
dnmartel <marteld1992@gmail.com>
2022-09-19 14:39:03 -0300

    Correcciones

---
+++ modules/productos.js
@@ -0,0 +1,36 @@
+class Productos {
+    constructor() {
+        this.productos = [];
+    }
+
+    GetAll() {
+        return this.productos;
+    }
+
+    GetByID(id) {
+        return this.productos.find((e) => e.id === Number(id));
+    }
+
+    Save(objeto) {
+        if (this.productos.length === 0) {
+            objeto.id = 1;
+        } else {
+            objeto.id = this.productos[this.productos.length - 1].id + 1;
+        }
+        this.productos.push(objeto);
+        return objeto;
+    }
+
+    Update(id, data) {
+        const productoID = this.productos.find((e) => e.id === Number(id));
+        Object.assign(productoID, data);
+        return productoID;
+    }
+
+    Delete(id) {
+        const toDelete = this.GetByID(id);
+        this.productos.splice(this.productos.indexOf(toDelete), 1);
+    }
+}
+
+module.exports = Productos;
--- routers/productos.js
+++ routers/productos.js
@@ -2,113 +2,84 @@
 const express = require("express");
 const { Router } = express;
 const router = Router();
+const Productos = require("../modules/productos");
 
 // Importo el contendor e inicializo un nuevo archivo - OPCIONAL
 /* const Contenedor = require("../modules/contenedor");
 const datosContenedor = new Contenedor("productos.txt"); */
 
-// Inicializo un array de productos en memoria
-const productos = [
+// Inicializo un array de productos en memoria, basado en clase
+const productos = new Productos();
+
+// Defino y pusheo productos de prueba
+const base = [
     {
         title: "Perro",
-        price: 300.45,
+        price: 500.45,
         thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        id: 1
-    },
-    {
-        title: "Escuadra2",
-        price: 123.49,
-        thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        id: 2
-    },
-    {
-        title: "Escuadra",
-        price: 123.49,
-        thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        id: 3
-    },
-    {
-        title: "Calculadora",
-        price: 234.56,
-        thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-        id: 4
+            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png"
     },
     {
         title: "Regla",
-        price: 223.25,
+        price: 123.49,
         thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        id: 5
+            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png"
     },
     {
-        title: "Globo Terráqueo",
-        price: 345.67,
+        title: "Escuadra",
+        price: 321.14,
         thumbnail:
-            "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-        id: 6
+            "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png"
     }
 ];
-
-// GET '/api/productos' -> devuelve todos los productos.
-router.get("/", (req, res) => {
-    res.status(200).json(productos);
+base.forEach((e) => {
+    productos.Save(e);
 });
 
-// GET '/api/productos/:id' -> devuelve un producto según su id.
+// GET '/api/productos' -> devuelve todos los productos. - CHECKEADO
+router.get("/", (req, res) => {
+    res.status(200).json(productos.GetAll());
+});
+
+// GET '/api/productos/:id' -> devuelve un producto según su id. - CHECKEADO
 router.get("/:id", (req, res) => {
-    if (productos[Number(req.params.id) - 1] === undefined) {
+    const productoID = productos.GetByID(req.params.id);
+
+    if (productoID === undefined) {
         res.status(404).json({ error: "Producto no encontrado" });
     } else {
-        res.status(200).json(productos[Number(req.params.id) - 1]);
+        res.status(200).json(productoID);
     }
 });
 
-// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado. - CHECKEADO
 router.post("/", (req, res) => {
     const data = req.body;
-    // Asigno ID 1 si no tiene contenido, sino, 1 más que el último
-    if (productos.length === 0) {
-        data.id = 1;
-    } else {
-        data.id = productos[productos.length - 1].id + 1;
-    }
-    // Pusheo el objeto al array
-    productos.push(data);
-    res.status(201).json(data);
+    res.status(201).json(productos.Save(data));
 });
 
 // PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
 router.put("/:id", (req, res) => {
-    if (productos[Number(req.params.id) - 1] === undefined) {
+    const productoID = productos.GetByID(req.params.id);
+
+    if (productoID === undefined) {
         res.status(404).json({ error: "Producto no encontrado" });
     } else {
-        const id = Number(req.params.id);
-        const data = req.body;
-        Object.assign(productos[id - 1], data);
-        res.status(202).json(productos[id - 1]);
+        const updated = productos.Update(req.params.id, req.body);
+        res.status(202).json(updated);
     }
 });
 
 // DELETE '/api/productos/:id' -> elimina un producto según su id.
 router.delete("/:id", (req, res) => {
-    if (productos[Number(req.params.id) - 1] === undefined) {
+    const productoID = productos.GetByID(req.params.id);
+    console.log(productoID);
+    if (productoID === undefined) {
         res.status(404).json({ error: "Producto no encontrado" });
     } else {
-        const id = req.params.id;
-        productos.splice(id - 1, 1);
+        productos.Delete(req.params.id);
         res.status(200).json("Objeto borrado");
     }
-});
-
-// EXTRA - Devuelve un producto random
-router.get("/random", (req, res) => {
-    const randBetween = Math.floor(Math.random() * productos.length) + 1;
-
-    res.json(productos(randBetween));
 });
 
 module.exports = router;
